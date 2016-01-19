325087cea23d970dd0a266c8804256d86d362e3b
Ben Johnson <Ben@dhcp-10-142-237-40.uawifi.arizona.edu>
2016-01-19 10:19:19 -0700

    Inital commit

---
+++ First_project.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Ben
+ *
+ * Created on January 19, 2016, 10:17 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
